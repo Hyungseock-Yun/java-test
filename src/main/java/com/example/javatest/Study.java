cf2a5de162d1f7eda1b3dd312439443b705e557a
Hyungseock-Yun <bagzein@gmail.com>
2022-05-09 02:57:19 +0900

    - JUnit5 테스트 이름 표시하기 - JUnit5 Assertion

--- src/main/java/com/example/javatest/Study.java
+++ src/main/java/com/example/javatest/Study.java
@@ -1,4 +1,16 @@
 package com.example.javatest;
 
+import lombok.Data;
+
+@Data
 public class Study {
+
+  private StudyStatus status = StudyStatus.DRAFT;
+
+  private int limit;
+
+  public Study(int limit) {
+    if (limit < 0) throw new IllegalStateException("limit은 0보다 커야 한다");
+    this.limit = limit;
+  }
 }
--- src/test/java/com/example/javatest/StudyTest.java
+++ src/test/java/com/example/javatest/StudyTest.java
@@ -2,21 +2,38 @@ package com.example.javatest;
 
 import org.junit.jupiter.api.*;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {   // JUnit5에서는 public을 선언하지 않아도 된다.
 
   @Test
-  void creat() {
-    Study study = new Study();
-    assertNotNull(study);
+  @DisplayName("스터디 만들기")
+  void creat_new_study() {
+    Study study = new Study(10);
+    assertAll(
+      () -> assertNotNull(study),
+      () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값이 DRAFT여야 한다."),   // lamda식을 쓰면 문자열연산을 성공시에만 하기 때문에 비용을 절감할 수 있음.
+      () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다."),
+      () -> assertTimeout(Duration.ofMillis(100), () -> {
+        new Study(10);
+        Thread.sleep(300);
+      }),
+      () -> assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+        new Study(10);
+        Thread.sleep(300);    // TODO ThreadLocal
+      })
+    );
+
     System.out.println("create");
   }
 
   @Test
   @Disabled
-  void creat1() {
-    Study study = new Study();
+  void creat_new_study_again() {
+    Study study = new Study(10);
     assertNotNull(study);
     System.out.println("create1");
   }
