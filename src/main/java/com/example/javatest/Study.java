b36a4757d491e78595c6b4224ebcf5c1b4d4c8d2
Hyungseock-Yun <bagzein@gmail.com>
2022-05-12 02:36:04 +0900

    - JUnit5 테스트 반복하기 - JUnit5 테스트 인스턴스

--- src/main/java/com/example/javatest/Study.java
+++ src/main/java/com/example/javatest/Study.java
@@ -9,6 +9,13 @@ public class Study {
 
   private int limit;
 
+  private String name;
+
+  public Study(int limit, String name) {
+    this.limit = limit;
+    this.name = name;
+  }
+
   public Study(int limit) {
     if (limit < 0) throw new IllegalStateException("limit은 0보다 커야 한다");
     this.limit = limit;
--- src/test/java/com/example/javatest/StudyTest.java
+++ src/test/java/com/example/javatest/StudyTest.java
@@ -2,6 +2,16 @@ package com.example.javatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
 
 import java.time.Duration;
 
@@ -9,7 +19,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)   // @BeforeAll, @AfterAll에 static을 붙이지 않아도 된다.
 class StudyTest {   // JUnit5에서는 public을 선언하지 않아도 된다.
+
+  int value = 1;    // @TestInstance(TestInstance.Lifecycle.PER_CLASS)를 사용하면 해당 변수를 어느 테스트에서 사용하든 값이 변경되지 않는다.
 
   @Test
   @DisplayName("스터디 만들기")
@@ -75,6 +88,45 @@ class StudyTest {   // JUnit5에서는 public을 선언하지 않아도 된다.
   @Tag("slow")
   void test_tag_slow() {
     System.out.println("slow");
+  }
+
+  @DisplayName("스터디 만들기")
+  @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+  void repeatTest(RepetitionInfo repetitionInfo) {
+    System.out.println("test " + repetitionInfo.getCurrentRepetition() + "/" +
+      repetitionInfo.getTotalRepetitions());
+  }
+
+  @DisplayName("스터디 만들기")
+  @ParameterizedTest(name = "{index} {displayName} message={0}")
+//  @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
+  @ValueSource(ints = {10, 20, 40})
+  void parameterizedTest(@ConvertWith(StudyConverter.class) Study study) {
+    System.out.println(study.getLimit());
+  }
+
+  @DisplayName("스터디 만들기")
+  @ParameterizedTest(name = "{index} {displayName} message={0}")
+  @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+  void parameterizedTest2(@AggregateWith(StudyAggregator.class) Study study) {
+    System.out.println(study);
+  }
+
+  static class StudyAggregator implements ArgumentsAggregator {
+
+    @Override
+    public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) throws ArgumentsAggregationException {
+      return new Study(accessor.getInteger(0), accessor.getString(1));
+    }
+  }
+
+  static class StudyConverter extends SimpleArgumentConverter {
+
+    @Override
+    protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+      assertEquals(Study.class, targetType, "Can only convert to Study");
+      return new Study(Integer.parseInt(source.toString()));
+    }
   }
 
   @FastTest
